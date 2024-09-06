051a74b9f59d2e23801129cc2ac52ef708c0ef18
Pranav M <pranavm265@gmail.com>
2024-09-06 16:03:50 +0530

    initial commit

---
+++ lib/pages/splash.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  bool _screenChanger = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 5), () {
+      // if (mounted) {
+        setState(() {
+          _screenChanger = true;
+        });
+      // }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: _screenChanger 
+            ? const Text('New Screen')
+            : LoadingAnimationWidget.threeRotatingDots(
+                color: Colors.white,
+                size: 100,
+              ),
+      ),
+    );
+  }
+}
